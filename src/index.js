1562c5ecb794b9007c39e42d433e470c5f8612d4
Krzysztof Wiskowski <krzysztof.wiskowski@gmail.com>
2023-09-18 23:28:28 +0200

    set up gallery

--- src/index.js
+++ src/index.js
@@ -1,36 +1,114 @@
 import axios from 'axios';
-// axios.defaults.headers.common['key'] = '39429562-362aa611c83bf0adbf53209b3';
-
 import Notiflix from 'notiflix';
 Notiflix.Notify.init({
   position: 'center-top',
 });
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const inputEl = document.querySelector('input[name="searchQuery"]');
+const btnEl = document.querySelector('button[type="submit"]');
+const galleryEl = document.querySelector('.gallery');
 
 const API_KEY = '39429562-362aa611c83bf0adbf53209b3';
-const URL =
-  'https://pixabay.com/api/?key=' +
-  API_KEY +
-  '&q=' +
-  encodeURIComponent('red roses');
-$.getJSON(URL, function (data) {
-  if (parseInt(data.totalHits) > 0)
-    $.each(data.hits, function (i, hit) {
-      console.log(hit.pageURL);
-    });
-  else console.log('No hits');
+const API_URL = 'https://pixabay.com/api/?';
+const lightbox = new SimpleLightbox('.gallery a');
+
+let page = 1;
+
+const searchApi = async () => {
+  return await axios.get(API_URL, {
+    params: {
+      key: API_KEY,
+      q: inputEl.value,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: page,
+      per_page: 40,
+    },
+  });
+};
+
+const loadApi = () => {
+  searchApi()
+    .then(response => {
+      if (response.data.hits.length > 0 && inputEl.value !== '') {
+        const totalHits = response.data.total;
+
+        galleryEl.innerHTML = createGallery(response);
+
+        Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
+        lightbox.refresh();
+      } else {
+        clear();
+        Notiflix.Notify.failure(
+          `Sorry, there are no images matching your search query. Please try again.`
+        );
+      }
+    })
+    .catch(error => console.log(error));
+};
+
+btnEl.addEventListener('click', e => {
+  e.preventDefault();
+  loadApi();
 });
 
-// function searchImages() {
-//   const query = document.querySelector('input.searchQuery');
-//   query.addEventListener('submit', searchImages);
-//   return axios
-//     .get(
-//       `https://pixabay.com/api/?q=${query}&image_type=photo&orientation=horizontal&safesearch=true`
-//     )
-//     .then(response => response.data)
-//     .catch(error => {
-//       console.error(error);
-//     });
-// }
+const clear = () => {
+  galleryEl.innerHTML = '';
+};
 
-// console.log(searchImages());
+const createGallery = response => {
+  return response.data.hits
+    .map(picture => {
+      return `<div class="photo-card">
+        <a href="${picture.largeImageURL}">
+        <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" /></a>
+          <div class="info">
+            <p class="info-item"><b>Likes</b>
+              ${picture.likes}
+            </p>
+            <p class="info-item"><b>Views</b>
+              ${picture.views}
+            </p>
+            <p class="info-item"><b>Comments</b>
+              ${picture.comments}
+            </p>
+            <p class="info-item"><b>Downloads</b>
+              ${picture.downloads}
+            </p>
+          </div>
+        </div>`;
+    })
+    .join('');
+};
+
+const loadMoreApi = () => {
+  page++;
+  searchApi().then(response => {
+    galleryEl.insertAdjacentHTML('beforeend', createGallery(response));
+    galleryEl.addEventListener('click', e => e.preventDefault());
+
+    lightbox.refresh();
+
+    const { height: cardHeight } = document
+      .querySelector('.gallery')
+      .firstElementChild.getBoundingClientRect();
+
+    if (response.data.total / page < 40) {
+      Notiflix.Notify.failure(
+        `We're sorry, but you've reached the end of search results.`
+      );
+    }
+  });
+};
+
+window.addEventListener('scroll', () => {
+  if (
+    window.scrollY + window.innerHeight >=
+    document.documentElement.scrollHeight
+  ) {
+    loadMoreApi();
+  }
+});
